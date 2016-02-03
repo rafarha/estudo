a97c87e83e7b420ec96219f83aa75f92d3744827
rafarha <rafael.rha@gmail.com>
2016-02-03 14:38:40 -0200

    Novo exercício a ser resolido

---
+++ EstudosCodility/src/com/codility/trainning/LessonThree/TapeEquilibriumTest.java
@@ -0,0 +1,9 @@
+package com.codility.trainning.LessonThree;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by rafael.alves on 03/02/2016.
+ */
+public class TapeEquilibriumTest extends TestCase {
+}
