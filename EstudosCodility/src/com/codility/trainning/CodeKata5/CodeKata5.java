9bbde76af30ab188ab56ea737c09520981eab50a
rafarha <rafael.rha@gmail.com>
2016-05-19 17:52:14 -0300

    Code Kata 5 Estrutura

--- EstudosCodility/src/com/codility/trainning/CodeKata5/CodeKata5.java
+++ EstudosCodility/src/com/codility/trainning/CodeKata5/CodeKata5.java
@@ -1,5 +1,6 @@
 package com.codility.trainning.CodeKata5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +9,37 @@ import java.util.List;
 public class CodeKata5 implements INumeroErdosService {
     @Override public Integer descobrirNumeroErdosDoAutor(final String pNomeAutor, final List<String> pAutoresArtigos)
 		    throws IllegalArgumentException {
+	List<String> listaAutoresClassificados = new ArrayList<String>();
+	String delemiters = "\\,";
+	int resultado = 0;
+	/*
 	for (int i = 0; i < pAutoresArtigos.size(); i++) {
 	    String autoresPorArtigo = pAutoresArtigos.get(i).toString();
+	    if (autoresPorArtigo.contains("P. Erdos")) {
+		String linha[] = autoresPorArtigo.split(delemiters);
+		if (linha != null) {
+		    Integer aux = 0;
+		    while (aux < linha.length) {
+			if (linha[aux].toString().equalsIgnoreCase("P. Erdos")) {
+			    listaAutoresClassificados.add(linha[aux] + "0");
+			} else {
+			    listaAutoresClassificados.add(linha[aux] + "1");
+			}
+			aux++;
+		    }
+		}
+	    }
+	    System.out.println("Teste");
+	} */
+	for (int i = 0; i < pAutoresArtigos.size(); i++) {
+	    String autoresPorArtigo = pAutoresArtigos.get(i).toString();
+	    if (autoresPorArtigo.contains(pNomeAutor)) {
+		if (autoresPorArtigo.contains("P. Erdos")) {
+		    resultado = 1;
+		    break;
+		}
+	    }
 	}
-
-	return null;
+	return resultado;
     }
 }
